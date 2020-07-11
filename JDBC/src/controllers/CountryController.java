cea5f3f5d6cd9a51633b113c186d56e9d9eeefaa
William Irsal <l0calh34rtz@gmail.com>
2020-07-11 21:28:30 +0700

    Technical documentation of controllers added

--- JDBC/src/controllers/CountryController.java
+++ JDBC/src/controllers/CountryController.java
@@ -21,12 +21,23 @@ public class CountryController {
         this.cdao = new CountryDAO();
     }
     
+    /**
+     * Retrieving all countries.
+     * @return List
+     */
     public List<Country> binding() {
         List<Country> countries = new ArrayList<>();
         countries = this.cdao.getCountries();
         return countries;
     }
     
+    /**
+     * Adding a country
+     * @param id
+     * @param name
+     * @param region
+     * @return String
+     */
     public String save(String id, String name, String region) {
         String message = "Simpan data gagal";
         int regionId = Integer.parseInt(region);
@@ -35,7 +46,14 @@ public class CountryController {
         return message;
     }
     
-    public String editName(String id, String name, String region) {
+    /**
+     * Updating a country
+     * @param id
+     * @param name
+     * @param region
+     * @return String
+     */
+    public String edit(String id, String name, String region) {
         String message = "Perbarui data gagal";
         int regionId = Integer.parseInt(region);
         Country country = new Country(id, name, regionId);
@@ -43,14 +61,11 @@ public class CountryController {
         return message;
     }
     
-//    public String editRegion(String id, String name, String region) {
-//        String message = "Simpan data gagal";
-//        int regionId = Integer.parseInt(region);
-//        Country country = new Country(id, name, regionId);
-//        if (this.cdao.updateCountryName(country)) message = "Simpan data berhasil";
-//        return message;
-//    }
-    
+    /**
+     * Deleting a country
+     * @param id
+     * @return String
+     */
     public String delete(String id){
         String result = "Hapus data gagal";
         Country country = new Country(id);
--- JDBC/src/controllers/RegionController.java
+++ JDBC/src/controllers/RegionController.java
@@ -33,6 +33,12 @@ public class RegionController {
 //        return this.rdao.getRegions();
     }
     
+    /**
+     * Adding a new region
+     * @param id 
+     * @param name
+     * @return String 
+     */
     public String save(String id, String name){
         String result = "Simpan data gagal";
 //        use this if id is not setted auto increment.
@@ -44,14 +50,26 @@ public class RegionController {
         return result;
     }
     
+    /**
+     * edit a region
+     * @param id
+     * @param name
+     * @return String 
+     */
+    
     public String edit(String id, String name){
-        String result = "Simpan data gagal";
+        String result = "Perbarui data gagal";
         int regionId = Integer.parseInt(id);
         Region region = new Region(regionId,name);
-        if (this.rdao.update(region)) result = "Simpan data berhasil";
+        if (this.rdao.update(region)) result = "Perbarui data berhasil";
         return result;
     }
     
+    /**
+     * delete a region
+     * @param id
+     * @return String 
+     */
     public String delete(String id){
         String result = "Hapus data gagal";
         int regionId = Integer.parseInt(id);
